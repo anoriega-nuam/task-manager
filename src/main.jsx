dfaf81b891191be13c4c45842372228bf49e2771
anoriega-nuam <ayriel.noriega@nuamx.com>
2025-02-07 17:31:11 -0500

    feat: add LoginPage and TaskPage components

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles.css'
 import { TaskApp } from './TaskApp.jsx'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>\
-      <Routes>
-        <Route path="/" element={<TaskApp />} />
-      </Routes>
+    <BrowserRouter>
+      <TaskApp />
     </BrowserRouter>
   </StrictMode>,
 )
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,9 +1,22 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import { LoginPage } from "../auth";
+import { TaskPage } from "../task";
 
 export const AppRouter = () => {
 
   const authStatus = 'not-authenticated';
 
   return (
-    <div>AppRouter</div>
+    <Routes>
+      {
+        (authStatus === 'not-authenticated')
+          ? <Route path="/" element={<LoginPage />} />
+          : <Route path="/*" element={<TaskPage />} />
+
+
+      }
+      <Route path="/*" element={<Navigate to="/auth/login" />} />
+
+    </Routes>
   )
 }
